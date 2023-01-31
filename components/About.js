e19ad3f098e705b74aed64fafa475173f87b736c
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-01-31 16:03:57 +0300

    Added padding

--- components/About.js
+++ components/About.js
@@ -10,7 +10,7 @@ const About = () => {
         <section className="flex justify-around align-center  flex-col md:flex-row">
           {/* Right side */}
           <div className="md:shadow-2xl md:shadow-red-500 flex justify-center items-center ">
-            <Image src={aboutimage} alt="aboutimage" className=" w-96  md:rounded-lg rounded-lg" />
+            <Image src={aboutimage} alt="aboutimage" className=" w-96 px-2 my-3 md:rounded-lg rounded-lg" />
           </div>
           {/* Left side  */}
                   <div className="ml-10 ">

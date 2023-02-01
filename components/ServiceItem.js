07590b4dbb90d5041dc4bdd02e76c75e015b441d
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-02-01 12:07:57 +0300

    Fixed slider

---
+++ components/ServiceItem.js
@@ -0,0 +1,17 @@
+
+// Import Swiper styles
+import 'swiper/css';
+const ServiceItem = () => {
+  return (
+      <div className='bg-white  shadow-xl p-4 h-96'>
+          vbnm
+    </div>
+
+  )
+}
+
+export default ServiceItem
+
+
+
+
--- components/Services.js
+++ components/Services.js
@@ -7,11 +7,10 @@ import { FaRunning } from "react-icons/fa";
 import { GiMuscleUp } from "react-icons/gi";
 import { GiHealthPotion } from "react-icons/gi";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay ,Pagination} from "swiper";
 const Services = () => {
   return (
     <div className="service-img bg-cover bg-center bg-fixed h-[700px] w-full">
-      <section className="flex flex-col justify-start md:ml-10 pt-10">
+      <section className="flex flex-col justify-start ml-10 pt-10">
         <h1 className="flex text-4xl text-white flex-row">
           {" "}
           <AiOutlineLine className="text-4xl mx-2 text-white" /> Our Services
@@ -23,15 +22,9 @@ const Services = () => {
       </section>
 
       <section className="mt-10 max-w-[1240px] m-auto">
-       
         <Swiper
-          modules={[Autoplay ,Pagination]}
           spaceBetween={20}
           slidesPerView={1}
-          slidesPerView={1}
-          spaceBetween={50}
-          draggable={true}
-          pagination={{ clickable: true }}
           loop="true"
           autoplay={{
             delay: 2500,
@@ -55,22 +48,7 @@ const Services = () => {
           }}
         >
           <SwiperSlide>
-            <div className="bg-white  shadow-xl p-4 h-80 mx-4 md:mx-0">
-              <div className="flex flex-col justify-center items-center">
-                <GiMuscleUp className="text-6xl text-gray-500" />
-                <h1 className="text-2xl font-bold text-gray-500">
-                  Muscle Building
-                </h1>
-                <p className="text-gray-500 text-center">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Voluptates, quod.
-                </p>
-                
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="bg-white  shadow-xl p-4 h-80 mx-4 md:mx-0">
+            <div className="bg-white  shadow-xl p-4 h-80 md:mx-0 mx-4">
               <div className="flex flex-col justify-center items-center">
                 <GiWeightLiftingUp className="text-6xl text-gray-500" />
                 <h1 className="text-2xl font-bold text-gray-500">
@@ -84,7 +62,49 @@ const Services = () => {
             </div>
           </SwiperSlide>
           <SwiperSlide>
-            <div className="bg-white  shadow-xl p-4 h-80 mx-4 md:mx-0">
+            <div className="bg-white  shadow-xl p-4 h-80 md:mx-0 mx-4">
+              <div className="flex flex-col justify-center items-center">
+                <GiHealthPotion className="text-6xl text-gray-500" />
+                <h1 className="text-2xl font-bold text-gray-500">
+                 Cardio Vascular Training
+                </h1>
+                <p className="text-gray-500 text-center">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Voluptates, quod.
+                </p>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="bg-white  shadow-xl p-4 h-80 md:mx-0 mx-4">
+              <div className="flex flex-col justify-center items-center">
+                <FaRunning className="text-6xl text-gray-500" />
+                <h1 className="text-2xl font-bold text-gray-500">
+                  Cardio Training
+                </h1>
+                <p className="text-gray-500 text-center">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Voluptates, quod.
+                </p>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="bg-white  shadow-xl p-4 h-80  md:mx-0 mx-4">
+              <div className="flex flex-col justify-center items-center">
+                <GiWeightLiftingUp className="text-6xl text-gray-500" />
+                <h1 className="text-2xl font-bold text-gray-500">
+                  Weight Loss
+                </h1>
+                <p className="text-gray-500 text-center">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Voluptates, quod.
+                </p>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="bg-white  shadow-xl p-4 h-80  md:mx-0 mx-4">
               <div className="flex flex-col justify-center items-center">
                 <MdOutlineFoodBank className="text-6xl text-gray-500" />
                 <h1 className="text-2xl font-bold text-gray-500">Nutrition</h1>
@@ -97,7 +117,7 @@ const Services = () => {
           </SwiperSlide>
 
           <SwiperSlide>
-            <div className="bg-white  shadow-xl p-4 h-80 mx-4 md:mx-0">
+            <div className="bg-white  shadow-xl p-4 h-80  md:mx-0 mx-4">
               <div className="flex flex-col justify-center items-center">
                 <BiDumbbell className="text-6xl text-gray-500" />
                 <h1 className="text-2xl font-bold text-gray-500">Strength</h1>

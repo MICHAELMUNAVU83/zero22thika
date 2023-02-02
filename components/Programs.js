1e00e4e806935296860028b46ff3cd5245d9e211
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-02-02 15:15:29 +0300

    Changed gap

--- components/Programs.js
+++ components/Programs.js
@@ -5,7 +5,7 @@ const Programs = () => {
   console.log(programmes);
   return (
     <div>
-      <section className=" mt-4 md:flex justify-around text-center container md:px-32 grid grid-cols-3 gap-12  px-3 ">
+      <section className=" mt-4 md:flex justify-around text-center container md:px-32 grid grid-cols-2 gap-x-48 gap-y-3 md:gap-x-0 px-3 ">
         <p className="bg-red-600 p-2 text-1xl text-white">Monday</p>
         <p className="bg-red-600 p-2 text-1xl text-white">Tuesday</p>
         <p className="bg-red-600 p-2 text-1xl text-white">Wednesday</p>

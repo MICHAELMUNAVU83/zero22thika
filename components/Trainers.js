92c3125d7a9fd3a9c49f549a1714b80768625b31
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-02-02 12:19:52 +0300

    Added trainers

--- components/Trainers.js
+++ components/Trainers.js
@@ -7,23 +7,27 @@ import trainer4 from "../public/trainer4.jpg";
 const Trainers = () => {
   return (
     <div className=" h-full">
-      <section className="flex flex-col justify-start ml-10 pt-10">
+      <section className="flex flex-col justify-start ml-10 pt-10 ">
         <h1 className="flex text-4xl  flex-row">
           {" "}
-          <AiOutlineDash className="text-4xl text-red-600 mx-2 " /> Our Team Members
+          <AiOutlineDash className="text-4xl text-red-600 mx-2 " /> Our Team
+          Members
         </h1>
         <h1 className=" font-bold text-4xl">MEET OUR MOST</h1>
         <h1 className=" font-bold text-4xl">EXPERIENCED TRAINERS</h1>
       </section>
-      <section className="flex justify-around flex-col items-center max-w-[1240px] mx-auto md:flex-row">
-        <div>
-          <Image src={trainer1} alt="trainer1" />
+      <section className="flex justify-around mb-10 mt-10 flex-col items-center max-w-[1240px] mx-auto md:flex-row">
+        <div className="shadow-xl shadow-red-500 flex flex-col my-4">
+          <Image src={trainer1} alt="trainer1" className="w-80 h-80 md:h-96" />
+          <p className="p-2 bg-red-600 text-center text-white">Michael Munavu</p>
         </div>
-        <div>
-          <Image src={trainer4} alt="trainer4" />
+        <div className="shadow-xl shadow-red-500 flex flex-col my-4">
+          <Image src={trainer4} alt="trainer4" className="w-80 h-80 md:h-96" />
+          <p className="p-2 bg-red-600 text-center text-white">Michael Munavu</p>
         </div>
-        <div>
-          <Image src={trainer3} alt="trainer3" />
+        <div className="shadow-xl shadow-red-500 flex flex-col my-4">
+          <Image src={trainer3} alt="trainer3" className="w-80 h-80 md:h-96" />
+          <p className="p-2 bg-red-600 text-center text-white">Michael Munavu</p>
         </div>
       </section>
     </div>

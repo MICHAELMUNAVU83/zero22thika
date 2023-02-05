b1534363a32071a6a4dcac4f06aee1dac214d000
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-02-05 13:32:56 +0300

    Added framer motion

--- components/Hero.js
+++ components/Hero.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { AiFillDownCircle } from "react-icons/ai";
 import Link from "next/link";
+import Image from "next/image";
+import logo1 from "../public/logo1.png";
 const Hero = () => {
   return (
     <div>
       <div className=" bg-cover custom-img bg-fixed h-[700px] w-full">
-        <div className="bg-black/60 h-full">
+        <div className="bg-gradient-to-r from-black/75 to-transparent h-full">
           <div className="flex justify-center flex-col items-center inset-0 z-10 h-full text-white  md:text-5xl  text-center  font-bold text-2xl">
-           
-            <h1 className="font-roboto font-bold mb-1">BUILD THE PERFECT BODY AND</h1>
+            <Image src={logo1} alt="logo" width={300} height={200} />
+            <h1 className="font-roboto font-bold mb-1">
+              BUILD THE PERFECT BODY AND
+            </h1>
             <h1 className="font-roboto font-bold text-red-600">
               THE PERFECT LIFE
             </h1>

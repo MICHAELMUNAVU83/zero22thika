88816d6787073183e6af8a753f7d68d6e96245e2
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-02-05 17:51:59 +0300

    Added rounded

--- components/Trainers.js
+++ components/Trainers.js
@@ -1,39 +1,63 @@
 import React from "react";
 import { CgGym } from "react-icons/cg";
 import Image from "next/image";
-import trainer3 from "../public/trainer3.jpg";
-import trainer4 from "../public/trainer4.jpg";
-import trainer2 from "../public/gymbro.jpg";
+import selfie1 from "../public/selfie1.png";
+import selfie3 from "../public/selfie3.png";
+import selfie4 from "../public/selfie4.png";
 const Trainers = () => {
   return (
     <div id="trainers" className=" h-full">
       <section className="flex flex-col justify-start ml-10 pt-10 ">
         <h1 className="flex text-4xl text-red-600 font-semibold  flex-row">
           {" "}
-          <CgGym className="text-4xl text-red-600 mx-2 " /> Our Team 
+          <CgGym className="text-4xl text-red-600 mx-2 " /> Our Team
         </h1>
         <h1 className=" font-bold  text-4xl">MEET OUR MOST</h1>
-        <h1 className=" font-bold  text-4xl">
-          EXPERIENCED TRAINERS
-        </h1>
+        <h1 className=" font-bold  text-4xl">EXPERIENCED TRAINERS</h1>
       </section>
       <section className="flex justify-around mb-10 mt-10 flex-col items-center max-w-[1240px] mx-auto md:flex-row">
-        <div className="shadow-xl shadow-red-500 flex flex-col my-4 hover:scale-105 transform transition duration-500">
-          <Image src={trainer2} alt="trainer1" className="w-80 h-80 md:h-96" />
-          <p className="p-2 bg-red-600 text-center text-white">
-            John Doe
+        <div className="flex justify-center relative bg-gradient-to-b   from-red-600  overflow-hidden rounded-full w-80 h-80 mt-5 hover:scale-110 transform transition-all duration-500 ease-in-out">
+          <Image
+            src={selfie1}
+            alt="kimani"
+            layout="fill"
+            objectFit="cover"
+            className="flex justify-center rounded-full"
+          />
+          <p className="absolute -bottom-32  w-48 h-48  text-white bg-gradient-to-b from-red-600 to-gray-100 rounded-full">
+            <span className="flex justify-center font-bold   p-4 text-2xl items-center ">
+              John Doe
+            </span>
           </p>
         </div>
-        <div className="shadow-xl shadow-red-500 flex flex-col my-4 hover:scale-105 transform transition duration-500">
-          <Image src={trainer4} alt="trainer4" className="w-80 h-80 md:h-96" />
-          <p className="p-2 bg-red-600 text-center text-white">
-           John Doe
+
+        <div className="flex justify-center relative bg-gradient-to-b   from-red-600  overflow-hidden rounded-full w-80 h-80 mt-5 hover:scale-110 transform transition-all duration-500 ease-in-out">
+          <Image
+            src={selfie3}
+            alt="kimani"
+            layout="fill"
+            objectFit="cover"
+            className="flex justify-center rounded-full"
+          />
+          <p className="absolute -bottom-32   w-48 h-48  text-white bg-gradient-to-b from-red-600 to-gray-100 rounded-full">
+            <span className="flex justify-center font-bold  p-4 text-2xl items-center ">
+              John Doe
+            </span>
           </p>
         </div>
-        <div className="shadow-xl shadow-red-500 flex flex-col my-4 hover:scale-105 transform transition duration-500">
-          <Image src={trainer3} alt="trainer3" className="w-80 h-80 md:h-96" />
-          <p className="p-2 bg-red-600 text-center text-white">
-           John Doe
+
+        <div className="flex justify-center relative bg-gradient-to-b   from-red-600  overflow-hidden rounded-full w-80 h-80 mt-5 hover:scale-110 transform transition-all duration-500 ease-in-out">
+          <Image
+            src={selfie4}
+            alt="kimani"
+            layout="fill"
+            objectFit="cover"
+            className="flex justify-center rounded-full"
+          />
+          <p className="absolute -bottom-32  w-48 h-48  text-white bg-gradient-to-b from-red-600 to-gray-100 rounded-full">
+            <span className="flex justify-center font-bold  p-4 text-2xl items-center ">
+              John Doe
+            </span>
           </p>
         </div>
       </section>
